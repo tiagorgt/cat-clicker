4d44a8337f8dc86e4cecc9f60a6f5b46975c6975
Tiago Ranghetti <tiago.rgt@outlook.com>
2017-08-29 20:56:32 -0300

    Changes in requirements

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,24 @@ var clickCountElem = document.getElementById('clickCount');
 var clickCount = 0;
 clickCountElem.textContent = clickCount;
 
-var elem = document.getElementById('catImg');
-elem.addEventListener('click', function () {
+var catName = 'Lui';
+var catName2 = 'Donatello';
+
+var catNameElem = document.getElementById('catName');
+var catName2Elem = document.getElementById('catName2');
+
+catNameElem.textContent = catName;
+catName2Elem.textContent = catName2;
+
+var imgElem = document.getElementById('catImg');
+imgElem.addEventListener('click', function () {
+    clickCount++;
+    clickCountElem.textContent = clickCount;
+});
+
+
+var img2Elem = document.getElementById('catImg2');
+img2Elem.addEventListener('click', function () {
     clickCount++;
     clickCountElem.textContent = clickCount;
 });
